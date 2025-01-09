ce4c4097821c166f1a344201114e74167a41c339
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-01-09 14:50:48 -0300

    configurações do H2, perfil de test e seeding da tabela Category

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- Inserindo categorias para o PDV da lanchonete
+INSERT INTO tb_category (name, description) VALUES ('Pastéis', 'Diversos sabores de pastéis salgados');
+INSERT INTO tb_category (name, description) VALUES ('Bebidas', 'Bebidas variadas como refrigerantes, sucos e água');
+INSERT INTO tb_category (name, description) VALUES ('Bebidas Alcoólicas', 'Cervejas e outras bebidas alcoólicas');
+INSERT INTO tb_category (name, description) VALUES ('Acompanhamentos', 'Acompanhamentos como batata frita e porções');
